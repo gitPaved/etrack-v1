4deea5ce7d92f7519ad983601e3d1bb63f83cad8
Paved <pavedngoune@gmail.com>
2025-03-03 17:28:52 +0100

    Init

---
+++ .history/app/page_20250303155835.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div><button className="btn btn-accent"> test</button></div>
+  );
+}
---
+++ .history/app/page_20250303162129.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-between flex-col py-10 w-full">
+      <div>
+        <div className="flex flex-col">
+          <h1 className="text-4xl md:text-5xl font-bold text-center">
+            Prenez le controle <br /> de vos finances
+          </h1>
+          <p className="">
+            Suivez vos budgets et vos depenses <br /> en toute simplicite avec notre application intuitive !
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
