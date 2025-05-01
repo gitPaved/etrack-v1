1c678bb0814dabf9f09bf4feed525a0da7393e2d
Paved <pavedngoune@gmail.com>
2025-05-01 17:01:50 +0100

    Graph

--- app/actions.ts
+++ app/actions.ts
@@ -260,3 +260,185 @@ export async function getTransactionsByEmailAndPeriod(email: string, period: str
 }
 
 //dashboard
+
+
+export async function getTotalTransactionAmount(email: string) {
+    try {
+        const user = await prisma.user.findUnique({
+            where: { email },
+            include: {
+                budgets: {
+                    include: {
+                        transactions: true
+                    }
+                }
+            }
+        })
+
+        if (!user) throw new Error("Utilisateur non trouvé");
+
+        const totalAmount = user.budgets.reduce((sum, budgets) => {
+            return sum + budgets.transactions.reduce((budjeSum, transaction) => budjeSum + transaction.amount, 0)
+        }, 0)
+
+        return totalAmount
+
+    } catch (error) {
+        console.error("Erreur lors du calcul du montant total des transactions:", error);
+        throw error;
+    }
+}
+
+
+export async function getTotalTransactionCount(email: string) {
+    try {
+        const user = await prisma.user.findUnique({
+            where: { email },
+            include: {
+                budgets: {
+                    include: {
+                        transactions: true
+                    }
+                }
+            }
+        })
+
+        if (!user) throw new Error("Utilisateur non trouvé");
+
+        const totalCount = user.budgets.reduce((count, budget) => {
+            return count + budget.transactions.length
+        }, 0)
+
+        return totalCount
+    } catch (error) {
+        console.error("Erreur lors du comptage des transactions:", error);
+        throw error;
+    }
+
+}
+
+
+export async function getReachedBudgets(email: string) {
+    try {
+        const user = await prisma.user.findUnique({
+            where: { email },
+            include: {
+                budgets: {
+                    include: {
+                        transactions: true
+                    }
+                }
+            }
+        })
+
+        if (!user) throw new Error("Utilisateur non trouvé");
+
+        const totalBudgets = user.budgets.length;
+        const reachedBudgets = user.budgets.filter(budjet => {
+            const totalTransactionsAmount = budjet.transactions.
+                reduce((sum, transaction) => sum + transaction.amount, 0)
+            return totalTransactionsAmount >= budjet.amount
+        }).length
+
+        return `${reachedBudgets}/${totalBudgets}`
+    } catch (error) {
+        console.error("Erreur lors du calcul des budgets atteints:", error);
+        throw error;
+    }
+
+}
+
+
+export async function getUserBudgetData(email: string) {
+    try {
+
+        const user = await prisma.user.findUnique({
+            where: { email },
+            include: { budgets: { include: { transactions: true } } },
+        });
+
+        if (!user) {
+            throw new Error("Utilisateur non trouvé.");
+        }
+
+        const data = user.budgets.map(budget => {
+            const totalTransactionsAmount = budget.transactions.reduce( (sum , transaction) => sum + transaction.amount ,0)
+            return {
+                budgetName: budget.name,
+                totalBudgetAmount : budget.amount,
+                totalTransactionsAmount
+            }
+        })
+        
+        return data
+
+    } catch (error) {
+        console.error("Erreur lors de la récupération des données budgétaires:", error);
+        throw error;
+    }
+}
+
+
+export const getLastTransactions = async (email: string) => {
+    try {
+        const transactions = await prisma.transaction.findMany({
+            where : {
+                budget : {
+                    user: {
+                       email : email 
+                    }
+                }
+            },
+            orderBy : {
+                createdAt: 'desc',
+            },
+            take: 10 , 
+            include: {
+                budget : {
+                    select: {
+                        name : true
+                    }
+                }
+            }
+
+        })
+
+        const transactionsWithBudgetName = transactions.map(transaction => ({
+            ...transaction,
+            budgetName: transaction.budget?.name || 'N/A', 
+        }));
+
+
+        return transactionsWithBudgetName
+
+    } catch (error) {
+        console.error('Erreur lors de la récupération des dernières transactions: ', error);
+        throw error;
+    }
+}
+
+export const getLastBudgets = async (email: string) => {
+    try {
+         const  budgets = await prisma.budget.findMany({
+            where : {
+                user : {
+                    email
+                }
+            },
+            orderBy: {
+                createdAt: 'desc',
+            },
+            take : 3,
+            include: {
+                transactions: true
+            }
+
+         })
+
+         return budgets
+
+    } catch (error) {
+        console.error('Erreur lors de la récupération des derniers budgets: ', error);
+        throw error;
+    }
+}
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -24,7 +24,7 @@ const Navbar = () => {
                         </div>
                         <div className="md:flex hidden">
                             <Link href={"/budgets"} className="btn ">Mes budgets</Link>
-                            <Link href={""} className="btn mx-4">Tableau de bord</Link>
+                            <Link href={"/dashboard"} className="btn mx-4">Tableau de bord</Link>
                             <Link href={"/transactions"} className="btn mx-4">Mes Transactions</Link>
                         </div>
                         <UserButton />
@@ -32,7 +32,7 @@ const Navbar = () => {
 
                     <div className="md:hidden flex justify-center mt-2">
                         <Link href={"/budgets"} className="btn btn-sm">Mes budgets</Link>
-                        <Link href={""} className="btn btn-sm mx-4">Tableau de bord</Link>
+                        <Link href={"/dashboard"} className="btn btn-sm mx-4">Tableau de bord</Link>
                         <Link href={"/transactions"} className="btn btn-sm mx-4">Mes Transactions</Link>
                     </div>
                 </>) : (<div className="flex items-center justify-between">
